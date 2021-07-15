597be6c529c14ba26dd587afb102018e38842c84
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-15 04:18:20 -0500

    api added

---
+++ src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default {
+    getEmployees: count => {
+        return axios.get(`https://randomuser.me/api/?results=${count}&inc=name,email&nat=us`);
+    }
+}
