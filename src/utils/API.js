78fb7ebc1167163e932114f59fa75ad507133c4f
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-15 04:13:01 -0500

    restarted file structure, couldnt compile

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/";
-const APIKEY = "&apikey=8HCN-RAS8-TYP6-GW3Y";
-
-export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
-  }
-};
