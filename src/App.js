f1edd74045951d468fa3a085184daa3db654e197
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-15 04:30:06 -0500

    index and app fixed in src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from "./components/container";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Directory</h1>
+      <hr />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
